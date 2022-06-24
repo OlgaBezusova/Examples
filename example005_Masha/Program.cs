b3a997ddaa0515d8838a7b139b039c85ace6b30b
OlgaBezusova <bezusova22@yandex.ru>
2022-06-24 17:46:15 +0300

    Masha

---
+++ example005_Masha/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("ведите имя пользователя");
+string username = Console.ReadLine();
+
+if (username == "Маша");
+{
+    Console.WriteLine("Ура, это же Маша!");
+}
+
+else 
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
+
+
