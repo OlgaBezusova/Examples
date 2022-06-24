7a2d1ea9a117e6b3581f09c686e0a0d625be87b1
OlgaBezusova <bezusova22@yandex.ru>
2022-06-24 23:19:45 +0300

    программа для МАХ

--- example006_max/Program.cs
+++ example006_max/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a = 1;
+int b = 8;
+int c = 6;
+int d = 4; 
+int e = 2;
+
+int max = a;
+
+if ( a > max) max = a;
+if ( b > max) max = b;
+if ( c > max) max = c;
+if ( d > max) max = d;
+if ( e > max) max = e;
+
+Console.Write("max= ");
+Console.WriteLine(max);
+
+
+
+
+
